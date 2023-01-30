0bd98ead2da8ab89a5a97e9c510009b05cc0b0e5
Sanguyen.24 <nsang4625@gmail.com>
2023-01-30 15:43:00 +0700

    set up db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
-// attach socket to http server
-const io = socketio(server);
-// create a new connection 
+
 io.on('connection', (socket) => {
   console.log('We had a new connection');
   socket.on('disconnect', () => {
